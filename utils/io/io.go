6742fa2ea88c1893b615991bea03cc97a5ab6af1
fatedier <fatedier@gmail.com>
2017-06-08 00:57:33 +0800

    io: WithCompression resuse snappy.Reader and snappy.Writer

--- utils/io/io.go
+++ utils/io/io.go
@@ -18,8 +18,6 @@ import (
 	"io"
 	"sync"
 
-	"github.com/golang/snappy"
-
 	"github.com/fatedier/frp/utils/crypto"
 	"github.com/fatedier/frp/utils/pool"
 )
@@ -55,8 +53,13 @@ func WithEncryption(rwc io.ReadWriteCloser, key []byte) (io.ReadWriteCloser, err
 }
 
 func WithCompression(rwc io.ReadWriteCloser) io.ReadWriteCloser {
-	return WrapReadWriteCloser(snappy.NewReader(rwc), snappy.NewWriter(rwc), func() error {
-		return rwc.Close()
+	sr := pool.GetSnappyReader(rwc)
+	sw := pool.GetSnappyWriter(rwc)
+	return WrapReadWriteCloser(sr, sw, func() error {
+		err := rwc.Close()
+		pool.PutSnappyReader(sr)
+		pool.PutSnappyWriter(sw)
+		return err
 	})
 }
 
@@ -64,13 +67,18 @@ type ReadWriteCloser struct {
 	r       io.Reader
 	w       io.Writer
 	closeFn func() error
+
+	closed bool
+	mu     sync.Mutex
 }
 
+// closeFn will be called only once
 func WrapReadWriteCloser(r io.Reader, w io.Writer, closeFn func() error) io.ReadWriteCloser {
 	return &ReadWriteCloser{
 		r:       r,
 		w:       w,
 		closeFn: closeFn,
+		closed:  false,
 	}
 }
 
@@ -83,6 +91,14 @@ func (rwc *ReadWriteCloser) Write(p []byte) (n int, err error) {
 }
 
 func (rwc *ReadWriteCloser) Close() (errRet error) {
+	rwc.mu.Lock()
+	if rwc.closed {
+		rwc.mu.Unlock()
+		return
+	}
+	rwc.closed = true
+	rwc.mu.Unlock()
+
 	var err error
 	if rc, ok := rwc.r.(io.Closer); ok {
 		err = rc.Close()
