db06ae77dcc03dc3c1d95a7961327a32cc543a79
Guy Lewin <guy.lewin@microsoft.com>
2020-02-17 11:25:36 +0200

    Update documentation

--- cmd/frpc/sub/tcphttptunnel.go
+++ cmd/frpc/sub/tcphttptunnel.go
@@ -1,4 +1,4 @@
-// Copyright 2018 fatedier, fatedier@gmail.com
+// Copyright 2020 guylewin, guy@lewin.co.il
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
