495ea523c291a8d672edb9f0b4294a2cd69291e7
hal-wang <hi@hal.wang>
2021-01-25 17:20:45 +0800

    add cacheGroups

--- web/vue.config.js
+++ web/vue.config.js
@@ -33,6 +33,41 @@ module.exports = {
       }
     }
   },
+  configureWebpack: config => {
+    config.optimization = {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: /node_modules/,
+            name: 'vendor',
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 100
+          },
+          common: {
+            chunks: 'all',
+            test: /[\\/]src[\\/]js[\\/]/,
+            name: 'common',
+            minChunks: 2,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 60
+          },
+          styles: {
+            name: 'styles',
+            test: /\.(sa|sc|c)ss$/,
+            chunks: 'all',
+            enforce: true
+          },
+          runtimeChunk: {
+            name: 'manifest'
+          }
+        }
+      }
+    }
+  },
   chainWebpack(config) {
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
