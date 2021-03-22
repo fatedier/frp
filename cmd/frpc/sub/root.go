9a849a29e94e969d99b41c8f3bb70c89ea46fdd3
fatedier <fatedier@gmail.com>
2021-03-22 14:53:30 +0800

    fix config parse logic (#2323)

--- cmd/frpc/sub/root.go
+++ cmd/frpc/sub/root.go
@@ -138,13 +138,11 @@ func parseClientCommonCfg(fileType int, source []byte) (cfg config.ClientCommonC
 	if err != nil {
 		return
 	}
-	if cfg.LogFile == "console" {
-		cfg.LogWay = "console"
-	} else {
-		cfg.LogWay = "file"
-	}
-	err = cfg.Check()
+
+	cfg.Complete()
+	err = cfg.Validate()
 	if err != nil {
+		err = fmt.Errorf("Parse config error: %v", err)
 		return
 	}
 	return
@@ -185,21 +183,20 @@ func runClient(cfgFilePath string) (err error) {
 	var content []byte
 	content, err = config.GetRenderedConfFromFile(cfgFilePath)
 	if err != nil {
-		return
+		return err
 	}
 
 	cfg, err := parseClientCommonCfg(CfgFileTypeIni, content)
 	if err != nil {
-		return
+		return err
 	}
 
 	pxyCfgs, visitorCfgs, err := config.LoadAllProxyConfsFromIni(cfg.User, content, cfg.Start)
 	if err != nil {
-		return
+		return err
 	}
 
-	err = startService(cfg, pxyCfgs, visitorCfgs, cfgFilePath)
-	return
+	return startService(cfg, pxyCfgs, visitorCfgs, cfgFilePath)
 }
 
 func startService(
