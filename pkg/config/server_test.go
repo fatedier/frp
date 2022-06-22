330991ac9b540f83b17a5b660fac1a7d89dd8bf2
berlin2123 <68841407+berlin2123@users.noreply.github.com>
2022-06-22 09:31:31 +0800

    Update server_test.go

--- pkg/config/server_test.go
+++ pkg/config/server_test.go
@@ -53,7 +53,7 @@ func Test_LoadServerCommonConf(t *testing.T) {
 				log_way = file
 				log_level = info9
 				log_max_days = 39
-				log_duration_types = ssh,rdp
+				log_duration_types = ssh
 				disable_log_color = false
 				detailed_errors_to_client
 				authentication_method = token
@@ -122,7 +122,7 @@ func Test_LoadServerCommonConf(t *testing.T) {
 				LogWay:                 "file",
 				LogLevel:               "info9",
 				LogMaxDays:             39,
-				LogDurationTypes:       "ssh,rdp",
+				LogDurationTypes:       "ssh",
 				DisableLogColor:        false,
 				DetailedErrorsToClient: true,
 				HeartbeatTimeout:       99,
