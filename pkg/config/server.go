0f60ef3ecb7bec73857f0aaf22c572fe9162a792
berlin2123 <68841407+berlin2123@users.noreply.github.com>
2022-10-13 10:06:56 +0800

    Update server.go

--- pkg/config/server.go
+++ pkg/config/server.go
@@ -115,10 +115,10 @@ type ServerCommonConf struct {
 	// value is 0.
 	LogMaxDays int64 `ini:"log_max_days" json:"log_max_days"`
 	// LogDurationTypes specifies the type of connection name, which will log
-        // the duration of those connections. if it is setting as 'ssh,rdp', it
-        // will log the duration of the name as 'ssh', 'ssh_1', 'sshname', 'rdp',
-        // 'rdp_test1' or 'web_my_rdp' . By default, this value is "ssh,rdp".
-        LogDurationTypes string `ini:"log_duration_types" json:"log_duration_types"`
+	// the duration of those connections. if it is setting as 'ssh,rdp', it
+	// will log the duration of the name as 'ssh', 'ssh_1', 'sshname', 'rdp',
+	// 'rdp_test1' or 'web_my_rdp' . By default, this value is "ssh,rdp".
+	LogDurationTypes string `ini:"log_duration_types" json:"log_duration_types"`
 	// DisableLogColor disables log colors when LogWay == "console" when set to
 	// true. By default, this value is false.
 	DisableLogColor bool `ini:"disable_log_color" json:"disable_log_color"`
