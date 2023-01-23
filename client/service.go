37ed8fb830011197afbb41b98bc5417e222dead5
Gerhard Tan <gwohau.tan@gmail.com>
2023-01-23 18:43:34 +0800

    Bugfix: add ipv6 parsing for custom DNS server

--- client/service.go
+++ client/service.go
@@ -114,8 +114,8 @@ func (svr *Service) Run() error {
 	// set custom DNSServer
 	if svr.cfg.DNSServer != "" {
 		dnsAddr := svr.cfg.DNSServer
-		if !strings.Contains(dnsAddr, ":") {
-			dnsAddr += ":53"
+		if _, _, err := net.SplitHostPort(dnsAddr); err != nil {
+			dnsAddr = net.JoinHostPort(dnsAddr, "53")
 		}
 		// Change default dns server for frpc
 		net.DefaultResolver = &net.Resolver{
