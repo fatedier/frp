8adf35d48059647c6f93e47d2431b8c7884a6735
wuqinqiang <1185079673@qq.com>
2023-12-21 10:53:27 +0800

    fix: close of closed channel

--- pkg/util/wait/backoff.go
+++ pkg/util/wait/backoff.go
@@ -16,6 +16,7 @@ package wait
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 
 	"github.com/samber/lo"
@@ -182,13 +183,15 @@ func Until(f func(), period time.Duration, stopCh <-chan struct{}) {
 
 func MergeAndCloseOnAnyStopChannel[T any](upstreams ...<-chan T) <-chan T {
 	out := make(chan T)
-
+	closeOnce := sync.Once{}
 	for _, upstream := range upstreams {
 		ch := upstream
 		go lo.Try0(func() {
 			select {
 			case <-ch:
-				close(out)
+				closeOnce.Do(func() {
+					close(out)
+				})
 			case <-out:
 			}
 		})
