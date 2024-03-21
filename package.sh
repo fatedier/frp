86f90f4d2709194c7537e7be4f4a6d4f379adbd4
fatedier <fatedier@gmail.com>
2024-03-21 19:38:18 +0800

    package.sh add android (#4094)

--- package.sh
+++ package.sh
@@ -17,7 +17,7 @@ make -f ./Makefile.cross-compiles
 rm -rf ./release/packages
 mkdir -p ./release/packages
 
-os_all='linux windows darwin freebsd'
+os_all='linux windows darwin freebsd android'
 arch_all='386 amd64 arm arm64 mips64 mips64le mips mipsle riscv64'
 
 cd ./release
