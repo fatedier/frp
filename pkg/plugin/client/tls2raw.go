8fd43167230fbb011be2bdf59382d2fce438e248
futrobo <futrobo@163.com>
2026-06-08 17:18:33 +0800

    fix(client): write proxy protocol header in tls2raw plugin

--- pkg/plugin/client/tls2raw.go
+++ pkg/plugin/client/tls2raw.go
@@ -72,6 +72,14 @@ func (p *TLS2RawPlugin) Handle(ctx context.Context, connInfo *ConnectionInfo) {
 		return
 	}
 
+	if connInfo.ProxyProtocolHeader != nil {
+		if _, err := connInfo.ProxyProtocolHeader.WriteTo(rawConn); err != nil {
+			xl.Warnf("tls2raw write proxy protocol header to local conn error: %v", err)
+			_ = rawConn.Close()
+			return
+		}
+	}
+
 	libio.Join(tlsConn, rawConn)
 }
 
